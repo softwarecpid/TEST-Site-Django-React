19b079670d5b446295c2e1240d05e0ec62b08dd9
Kazyel <m.mascarelo@gmail.com>
2023-11-14 08:52:30 -0300

    First push :fire:

---
+++ src/pages/root.tsx
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar/Navbar';
+
+function App() {
+  return (
+    <main className='grid h-screen w-full grid-cols-12 bg-zinc-900 font-ubuntu'>
+      <Navbar />
+    </main>
+  );
+}
+
+export default App;
